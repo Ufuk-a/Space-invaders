bb81f918387e7c1e26da57d0a0ddde320cbb4e69
TUNAPRO1234 <60389660+tunapro1234@users.noreply.github.com>
2020-10-28 17:43:36 +0300

    düzenlemeler

---
+++ Readme.md
@@ -0,0 +1,15 @@
+# Space Invader
+
+Öncelikle pygame.clock kullanmayı bilmiyorum onu bir araştır.
+
+`global` keywordünü kullanmaman iyi olabilir ama senin seçimin, ben başladığımda bokunu çıkarıyordum
+
+Her şeyin bir classı olmasını tercih ederim, ama player diye bir class yapıp enemy ve oyuncuyu onun objesi yapabilirsin.
+
+Her şeyin classından kastım Spaceship diye ana bir class daha sonra Enemy ve Player diye ondan türeyen classlar ve onun objeleri (player, enemy)
+
+res/glob.py dosyası dışında, her şeyi fonksiyonların içine yazmaya çalış.
+
+Mermi işi tamamen sana kalmış ya da oraya gelince konuşuruz
+
+takıl kafana göre
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from emptypygame.res.glob import *
+from space_invaders.res.glob import *
 import pygame
 
 
@@ -7,8 +7,8 @@ def main():
     pygame.display.set_caption(TITLE)
     clock = pygame.time.Clock()
 
-    screen_size = (WIDTH, HEIGHT)
-    screen_args = (screen_size, pygame.FULLSCREEN) if FULLSCREEN else (screen_size, )
+    scr_size = (WIDTH, HEIGHT)
+    screen_args = (scr_size, pygame.FULLSCREEN) if FULLSCREEN else (scr_size, )
     screen = pygame.display.set_mode(*screen_args)
 
     while True:
@@ -20,14 +20,13 @@ def main():
 
 def runTime(screen, clock):
     clock.tick(FPS)
-    
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             return False
 
         if pygame.mouse.get_pressed()[0]:
             pos = pygame.mouse.get_pos()
-            
 
         elif pygame.mouse.get_pressed()[2]:
             pos = pygame.mouse.get_pos()
