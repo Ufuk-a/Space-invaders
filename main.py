eff25f728ccc24c893c500572fce86d56d263e49
Ufuka <61376126+Ufuk-a@users.noreply.github.com>
2020-10-31 14:57:23 +0300

    Bastan yazdım
    Çok yazcak bişey yoktu ama bastan yazmayınca beynim çalısmadı nedense

--- main.py
+++ main.py
@@ -1,36 +1,32 @@
-from space_invaders.lib.player import Player
+from space_invaders.lib.classes import *
 from space_invaders.res.glob import *
 
 
 def main():
-    pygame.display.init()
+    
+    screen = pygame.display.set_mode((WIDTH, HEIGHT)) 
     pygame.display.set_caption(TITLE)
     clock = pygame.time.Clock()
-
-    # fullscreen meselesini neden yok ettin ki
-    screen = pygame.display.set_mode((WIDTH, HEIGHT))
     
-    # screen.fill((0, 0, 0))
+    ships = pygame.sprite.Group()
+    player = HumanShip(300)
+    ships.add(player)
     screen.fill(colors.black)
-
-    # oluşumları döngü içine koyarsan sürekli olarak baştan oluşturur
-    human_ship = Player(screen, x=0, health=100, ammo=100, is_enemy=False)
-    # sonra hareket edemezsin 
-
-    while True:
-        clock.tick(60)
-
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                break
-
-        # render ve update tarzı şeyleri eventlerle uğraştıktan sonra koy (burası)
-        human_ship.render()
+    
+    running = True
+    while running:
+        clock.tick(FPS)
         
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT: 
+                running = False 
+
+        ships.update()
+        ships.draw(screen)
         pygame.display.update()
-
-    pygame.quit()
-
+        
+    
+    pygame.quit() 
 
 if __name__ == "__main__":
     main()
