80b0950acfbcd3f51c5b93654dfbe5f4f7df3680
Ufuka <altintasufuk05@gmail.com>
2020-10-31 16:23:57 +0300

    enemy eklendi

--- space_invaders/lib/classes.py
+++ space_invaders/lib/classes.py
@@ -1,17 +1,53 @@
+from pygame.display import update
 from space_invaders.res.glob import *
 pygame.init()
-#büyük günah gibi geliyor ama baska türlü nasıl image lodlicam burda
 
+ships = pygame.sprite.Group()
 player_image = pygame.image.load("space_invaders\\res\\human_ship.png")
 
 class HumanShip(pygame.sprite.Sprite):
     
-    def __init__(self, x):
+    def __init__(self):
         pygame.sprite.Sprite.__init__(self)
         
         self.image = player_image
         self.rect = self.image.get_rect()
-        self.x = x
-        self.rect.center = (x, 500)
+        self.x = 300
+        self.rect.center = (self.x, 500)
         
+    def update(self):
+        key = pygame.key.get_pressed()
+        dist = 2
+        if key[pygame.K_LEFT]:
+            self.rect.x -= dist
+        elif key[pygame.K_RIGHT]:
+            self.rect.x += dist    
+            
+    def get_x(self):
+        return self.rect.x
+                
+player = HumanShip()
+            
+class EnemyShip(pygame.sprite.Sprite):
+    
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
         
+        self.image = pygame.transform.flip(player_image, False, True)
+        self.rect = self.image.get_rect()
+        self.x = 300
+        self.rect.center = (self.x, 100)                
+
+    def update(self):
+        
+        speed = 1.5 
+        if player.rect.x > self.rect.x:
+            self.rect.x += speed
+        elif player.rect.x < self.rect.x:
+            self.rect.x -= speed    
+        else:
+            pass
+            
+enemy = EnemyShip()
+
+ships.add(player, enemy)    
